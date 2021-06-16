2c90e4da02d10d05d4bfbc1043838682cac1c707
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-06-16 19:24:21 +0900

    Add: vue/cli

---
+++ todoList/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    Todo List
+    <HelloWorld msg="hi" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>

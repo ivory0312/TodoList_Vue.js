48acb168f295b5e3ea1f8f16ee3c8cc739517179
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-06-22 20:51:38 +0900

    help me

--- todoList/src/views/Home.vue
+++ todoList/src/views/Home.vue
@@ -1,19 +1,74 @@
 <template>
   <div class="home">
-    <div class="home-title">Todo List</div>
-    <TodoInput />
+    <div class="todo">
+      <div class="todo-title">Todo List</div>
+      <div class="todo-contents">
+        {{ input }}
+      </div>
+      <div class="todo-button">
+        <input v-model="input" class="todo-button-input" type="text" />
+        <button v-on:click="send()" class="todo-button-send">send</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import TodoInput from "../components/TodoInput/TodoInput.vue";
-import "./Home.scss";
-
 export default {
   name: "Home",
-  components: {
-    TodoInput,
+  data() {
+    return {
+      input: "",
+      list: [],
+    };
+  },
+  method: {
+    send: function() {
+      this.list.push(input);
+    },
   },
 };
+// console.log(data().input);
 </script>
+
+<style lang="scss" scoped>
+.todo {
+  width: 300px;
+  height: auto;
+  margin: 50px auto;
+  border: 1px solid #d2d2d2;
+  justify-content: center;
+  align-items: center;
+
+  &-title {
+    width: 100%;
+    height: 40px;
+    padding-top: 10px;
+    background-color: #0067bc;
+    color: white;
+    font-size: 24px;
+    text-align: center;
+  }
+  &-contents {
+    padding: 10px;
+  }
+  &-button {
+    padding: 10px;
+    border-top: 1px solid #d2d2d2;
+
+    &-input {
+      width: 220px;
+      border: none;
+    }
+    &-input:focus {
+      outline: none;
+    }
+    &-send {
+      border: none;
+      padding: 5px 10px;
+      color: white;
+      background-color: #0067bc;
+    }
+  }
+}
+</style>
